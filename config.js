9cd921379ed5b4d0f78822c556d25ba8351735c8
unknown <pnikkangelie@gmail.com>
2026-06-12 20:00:04 +0800

    Fix script URL and rebundle settings

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // ── Paste your Google Apps Script Web App URL below ────────
-var SCRIPT_URL = 'PASTE_YOUR_SCRIPT_URL_HERE'
+var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwjXCStQ3Yt0v_bLm8SKXZ-tQo2O7IolsNRtxcIRxbYPTXYjSEVU4RIYaAIHvSB8gA/exec'
